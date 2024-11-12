867c6079ede01749b95b7ed1157d77815376f855
erick-petcovikz <ericksiqueira690@gmail.com>
2024-11-12 16:57:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function alternarModo(){
+    document.body.classList.toggle('dark-mode');
+}
+// toggles são normalmente uma metáfora para interruptores de luz, que nos deixa escolher entre ligado e desligado
